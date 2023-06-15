3e0372f8146fcb4875b8d81109c3b630a75dca9d
JesVarAvi <jva_sm@hotmail.com>
2023-06-15 10:57:11 -0600

    Record author of post in the greetings table

--- lang/en/local_greetings.php
+++ lang/en/local_greetings.php
@@ -32,3 +32,4 @@ $string['greetingusermx'] = 'Hola, {$a}.';
 $string['greetinguserfj'] = 'Bula, {$a}.';
 $string['greetingusernz'] = 'Kia Ora, {$a}.';
 $string['yourmessage'] = 'Your message';
+$string['postedby'] = 'Posted by {$a}.';
